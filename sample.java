3898fa4cb8e18ebb9ae71e7254d21dba28675602
nskarthi <nskarthi@gmail.com>
2025-12-28 14:15:23 -0500

    Create sample.java

---
+++ sample.java
@@ -0,0 +1,9 @@
+package webelements;
+
+public class handlingTables {
+	public WebDriver driver = null;
+
+	public void handleTables() throws InterruptedException {
+			System.out.println(column.getText());
+	}
+}
